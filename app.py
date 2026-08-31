f007e6ccd96db1fb98ef44fa13682284cef8acce
zalimhash1 <abdalahmed870@gmail.com>
2026-08-31 09:41:30 +0500

    Connect frontend

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["http://127.0.0.1:5500",
+                   "https://electricity-production-predictor-1.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"]
 )
